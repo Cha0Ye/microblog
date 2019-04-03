6a451f220798a5d10494d506be2d6bb5c1c9e1b4
Chao Ye <Chao12.ye@gmail.com>
2019-04-03 10:25:06 -0700

    add app.js NewPostForm.js Post.js and PostList.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import { NavLink, Route, Switch } from 'react-router-dom'; 
+import PostList from './PostList';
+import NewPostForm from './NewPostForm';
+
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { posts: [] };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <nav>
+        <h1>Microblog</h1>
+        <p>Get in the Rithm of blogging!</p>
+        <NavLink to='/' >Blog</NavLink>
+        <NavLink to='/new' >Add a new post</NavLink>
+      </nav>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={ () => <PostList />} />
+            <Route exact path='/new' render={ () => <NewPostForm />} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
